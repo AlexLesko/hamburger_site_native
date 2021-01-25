3758a03b5f3f97fd6b21914e85f5348e59b24c7d
AlexLesko <l.alex0428@gmail.com>
2021-01-25 14:18:11 +0100

    first version

--- js/scrip.js
+++ js/scrip.js
@@ -1,20 +1,97 @@
-function render(){
-    return '' + `<div class="grid_item">
-        <img src="img/black_burger.jpg" alt="black_burger">
-        <div class="card_box_title">
-            <div class="title">Black Burger</div>
-            <div class="price">Price: 20$</div>
-        </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-            Donec tempor non tellus sed porttitor. Cras et augue a urna ultricies tempor vitae suscipit mauris.
-        </p>
-        <button>Order</button>
-    </div>` + '';
+function getSessions() {
+    return new Promise(function (resolve, reject) {
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function (e) {
+            resolve(e.target.response);
+        };
+        oReq.open('GET', 'datas.json', true);
+        oReq.responseType = 'json';
+        oReq.send();
+    })
 }
 
-function addMenu(){
+function render(datas) {
+    return '' + datas.map(function (item) {
+        return `<div class="grid_item">
+    <img src=${item.img} alt=${item.alt}>
+    <div class="card_box_title">
+        <div class="title">${item.title}</div>
+        <div class="price">Price: ${item.price}</div>
+    </div>
+    <p class="desc">${item.description}</p>
+    <button data-order="${item.selection}">Order</button>
+    </div>`}).join('') + '';
+}
+
+function showBestBurger(datas) {
+    return '' + datas.map(function (item) {
+        if (item.topBurger == "true") {
+            return `<div class="grid_item">
+    <img src=${item.img} alt=${item.alt}>
+    <div class="card_box_title">
+        <div class="title">${item.title}</div>
+        <div class="price">Price: ${item.price}</div>
+    </div>
+    <p>${item.description}</p>
+    </div>`}
+    }).join('') + '';
+}
+
+function addMenu() {
     let listItem = document.getElementById("gridcontainer");
-    listItem.innerHTML = render();
+    listItem.innerHTML = render(data.listItems);
 }
 
-addMenu();
+function addTopBurger() {
+    let topBurger = document.getElementById("topBurger");
+    topBurger.innerHTML = showBestBurger(data.listItems);
+}
+
+var data = {
+    listItems: []
+};
+
+getSessions()
+    .then((datas) => {
+        console.log('promises!')
+        data.listItems = datas;
+        addMenu();
+    });
+getSessions()
+    .then((datas) => {
+        console.log('promises!')
+        data.listItems = datas;
+        addTopBurger();
+    });
+
+setTimeout(function () {
+
+    const orderButton = document.querySelectorAll("button[data-order]");
+    console.log(orderButton);
+
+    orderButton.forEach(function (button) {
+        button.addEventListener("click", function (e) {
+            const button = e.currentTarget;
+            const container = button.parentNode;
+            console.log("test")
+            const order = {
+                id: button.getAttribute("data-order"),
+                title: container.querySelector(".title").innerText,
+                price: container.querySelector(".price").innerText,
+                desc: container.querySelector(".desc").innerText
+                
+            };
+
+            localStorage.setItem("order", JSON.stringify(order));
+
+            const url = window.location.href.replace("menu.html", "order.html");
+            window.location.href = url;
+
+        })
+    })
+
+}, 1000);
+
+
+
+
